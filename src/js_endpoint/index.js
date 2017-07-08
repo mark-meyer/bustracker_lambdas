cef21bf55f1ac074ef51c76c4d27ec57816b5a3c
Mark Meyer <mark@photo-mark.com>
2017-07-07 19:47:32 -0800

    Stringify return data through API

--- src/js_endpoint/index.js
+++ src/js_endpoint/index.js
@@ -118,7 +118,7 @@ function makeResponse(data) {
         "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
         "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
     },
-    "body": data
+    "body": JSON.stringify(data)
     }
 }
 
